f7129c1e7f24d869db6afeeb0f44340dc3d82440
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-10-22 17:31:05 +0200

    the 2ht stage done

--- weather-app-world/src/components/current-weather/current-weather.jsx
+++ weather-app-world/src/components/current-weather/current-weather.jsx
@@ -5,12 +5,54 @@ const CurrentWeather= ()=>{
   return (
     <div className="weather">
       <div className="top">
-        <p className="city">Berlin</p>
-        <p className="weather-description">Sunny</p>
-
+        <div>
+          <p className="city">Berlin</p>
+          <p className="weather-description">Sunny</p>
+        </div>
+        <img alt="weather" className="weather-icon" src="icons/01d.png"></img>
       </div>
-      <img alt="weather" className="weather-icon" src=""></img>
-
+      <div className="bottom">
+        <p className="temp">18°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label top">
+              Details
+            </span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">
+                Feel like
+              </span>
+              <span className="parameter-value">
+                19°C
+              </span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">
+                wind
+              </span>
+              <span className="parameter-value">
+                2 m/s
+              </span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">
+                Humidity
+              </span>
+              <span className="parameter-value">
+                15%
+              </span>
+          </div>
+          <div className="parameter-row">
+              <span className="parameter-label">
+                pressure
+              </span>
+              <span className="parameter-value">
+                15 hPa
+              </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
